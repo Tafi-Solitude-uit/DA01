6b70facfd2923af8184646b8e711fb2422c39ae5
Tafi-Solitude-uit <156486860+Tafi-Solitude-uit@users.noreply.github.com>
2024-01-16 19:20:49 +0700

    Create DAY7_EX.sql

---
+++ DAY7_EX.sql
@@ -0,0 +1,34 @@
+--Bài tập 1 
+SELECT NAME
+FROM STUDENTS 
+WHERE MARKS>75 
+ORDER BY RIGHT(NAME,3), ID ASC
+
+--Bài tập 2
+# Write your MySQL query statement below
+SELECT USER_ID, CONCAT(UPPER(LEFT(NAME,1)), LOWER(RIGHT(NAME,LENGTH(NAME)-1))) AS NAME
+FROM USERS
+ORDER BY USER_ID
+
+--Bài tập 3
+
+
+--Bài tập 4 
+
+
+--Bài tập 5
+
+
+--Bài tập 6 
+
+
+--Bài tập 7
+
+
+--Bài tập 8
+
+
+--Bài tập 9
+
+
+--Bài tập 10 
