a4c7a895abf76a3ed3d30b3b89e49c371c3172cd
Tafi-Solitude-uit <156486860+Tafi-Solitude-uit@users.noreply.github.com>
2024-01-31 15:09:50 +0700

    Update DAY19_project.sql

--- DAY19_project.sql
+++ DAY19_project.sql
@@ -18,7 +18,73 @@ alter table sales_dataset_rfm_prj
 ALTER COLUMN orderlinenumber TYPE smallint
 USING orderlinenumber::smallint;
 
-alter table sales_Câu 2
+alter table sales_dataset_rfm_prj
+ALTER COLUMN orderlinenumber TYPE smallint
+USING orderlinenumber::smallint;
+
+alter table sales_dataset_rfm_prj
+ALTER COLUMN sales TYPE numeric
+USING sales::numeric;
+--
+SET datestyle = 'ISO, MDY';
+
+alter table sales_dataset_rfm_prj
+ALTER COLUMN orderdate TYPE timestamp with time zone
+USING orderdate::timestamp with time zone;
+
+alter table sales_dataset_rfm_prj
+ALTER COLUMN status TYPE varchar(12)
+USING status::varchar(12);
+
+alter table sales_dataset_rfm_prj
+ALTER COLUMN productline TYPE varchar(30)
+USING productline::varchar(30);
+
+alter table sales_dataset_rfm_prj
+ALTER COLUMN msrp TYPE smallint
+USING msrp::smallint;
+
+alter table sales_dataset_rfm_prj
+ALTER COLUMN productcode TYPE varchar(12)
+USING productcode::varchar(12);
+
+alter table sales_dataset_rfm_prj
+ALTER COLUMN customername TYPE character varying
+USING customername::character varying;
+
+alter table sales_dataset_rfm_prj
+ALTER COLUMN phone TYPE varchar(30)
+USING phone::varchar(30)
+
+alter table sales_dataset_rfm_prj
+ALTER COLUMN addressline1 TYPE character varying
+USING addressline1::character varying;
+
+alter table sales_dataset_rfm_prj
+ALTER COLUMN city TYPE varchar(20)
+USING city::varchar(20);
+
+alter table sales_dataset_rfm_prj
+ALTER COLUMN state TYPE varchar(12)
+USING state::varchar(12);
+
+alter table sales_dataset_rfm_prj
+ALTER COLUMN postalcode TYPE varchar(12)
+USING postalcode::varchar(12);
+
+alter table sales_dataset_rfm_prj
+ALTER COLUMN country TYPE varchar(20)
+USING country::varchar(20);
+
+alter table sales_dataset_rfm_prj
+ALTER COLUMN territory TYPE varchar(20)
+USING territory::varchar(20);
+
+alter table sales_dataset_rfm_prj
+ALTER COLUMN dealsize TYPE varchar(12)
+USING dealsize::varchar(12);
+
+----Câu 2
 SELECT 
     *
 FROM 
